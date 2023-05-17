b4c4a18e20384f8f1b48c5c6f4f899f757f41436
jamleston <jamlessston@gmail.com>
2023-05-17 09:59:45 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,49 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+// refs and const
+const refs = {
+    galleryListRef: document.querySelector('.gallery')
+}
 
-const a = 'hello';
-const b = 5;
+const itemsMarkup = createArrayOfItems(galleryItems);
+
+// events
+
+refs.galleryListRef.insertAdjacentHTML("afterbegin", itemsMarkup);
+refs.galleryListRef.addEventListener('click', onOpenModal);
+
+// functions
+
+function createArrayOfItems (array) {
+    return array.map(elem =>{
+        return `<li class="gallery__item">
+        <a class="gallery__link" href=${elem.original}>
+        <img
+            class="gallery__image"
+            src=${elem.preview}
+            data-source=${elem.original}
+            alt=${elem.description}
+        />
+        </a>
+        </li>`
+    }).join('');
+};
+
+function onOpenModal (event) {
+    event.preventDefault();
+    const currentItem = event.target;
+
+    if(currentItem.nodeName !=='IMG') {
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img class='gallery__image' src='${currentItem.parentNode.href}' width="800" height="600"/>`);
+
+    instance.show();
+    console.log(instance);
+}
