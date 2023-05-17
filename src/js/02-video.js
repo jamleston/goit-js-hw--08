4267798c321115fad95a21eaa8e454b28cfc04a4
jamleston <jamlessston@gmail.com>
2023-05-17 10:55:44 +0200

    second task without lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+// variables
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
+
+// events
+player.on('timeupdate', throttle(updateTimeFn, 5000));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+
+// functions
+function updateTimeFn (data) {
+    localStorage.setItem(CURRENT_TIME, data.seconds);
+    console.log(data.seconds)
+}
+
 
